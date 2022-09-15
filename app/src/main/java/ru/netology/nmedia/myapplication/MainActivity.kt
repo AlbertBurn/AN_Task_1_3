c9583dc13cf19a69b3d6876ce8ecd1e58c29a0a3
Albert R. Burnyashov <albrburn@gmail.com>
2022-09-15 10:34:56 +0300

    Initial commit

---
+++ app/src/main/java/ru/netology/nmedia/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.netology.nmedia.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
